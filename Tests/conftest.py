05f2c239a9ae36678f4a3a5c25f8e162ce396cc1
rokitapiotr <rokitapiotr97@gmail.com>
2023-08-20 21:37:00 +0200

    conftest update

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,21 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.safari.service import Service as Safari
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-@pytest.fixture
+@pytest.fixture(params=["chrome", "firefox", "safari"])
 def driver(request):
     browser = request.config.getoption("--browser")
-    #browser = request.param
     print(f"Creating {browser} driver")
     if browser == "chrome":
         my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     elif browser == "firefox":
         my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif browser == "safari":
+        my_driver = webdriver.Safari(service=Safari(GeckoDriverManager().install()))
     else:
-        raise TypeError(f"Expected 'chrome' or 'firefox', but got {browser}")
-    # my_driver.implicitly_wait(10)
+        raise TypeError(f"Expected 'chrome', 'firefox' , 'safari', but got {browser}")
+    my_driver.implicitly_wait(10)
     yield my_driver
     print(f"Closing {browser} driver")
     my_driver.quit()
@@ -25,5 +27,5 @@ def driver(request):
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)"
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome,firefox and Safari)"
     )
